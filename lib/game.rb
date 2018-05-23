388582601aff9477a9c534a2de0c073d0f95f884
Grace Shih <vocalise@gmail.com>
2018-05-23 21:48:49 +0000

    Created Initialize function

---
+++ lib/game.rb
@@ -0,0 +1,31 @@
+require_relative "./player.rb"
+require_relative './players/computer.rb'
+require_relative './players/human.rb'
+require_relative './board.rb'
+
+class Game
+  extend Players
+  attr_accessor :board
+  attr_reader :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+  ]
+
+  def initialize(board=Board.new, player_1=Players::Human.new("X"), player_2=Players::Human.new("O"))
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+end
+
+g = Game.new
+g.board.display
