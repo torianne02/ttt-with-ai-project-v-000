7250d7f4dd3641d4cd57a0364b9f98d0c457b5aa
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-24 14:33:02 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -18,6 +18,7 @@ module Players
       end
       move
     end
+<<<<<<< HEAD
   end
 end
 
@@ -39,6 +40,8 @@ end
     #     move = "5"
     #   end
 
+=======
+>>>>>>> b6717fa8eeba55d6d453f7a60c6d9378cdfdf1cd
     # if win_combinations is possible, i.e. 1 move off --> not sure how to state this in code
     #   move = index of open move to win  --> to either win or block
 
@@ -48,3 +51,8 @@ end
     # check if there are any two positions that are taken with the same opposing token that is
     # lands in the win_combinations and block the third position that has not yet been
     # taken in the winning combination
+<<<<<<< HEAD
+=======
+  end
+end
+>>>>>>> b6717fa8eeba55d6d453f7a60c6d9378cdfdf1cd
